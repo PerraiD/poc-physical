24cacb6c920821f70fbcd1bddeaeffc734935ebc
david p <davidperrai@gmail.com>
2016-11-30 16:35:40 +0100

    changement des lien dynamiquement

--- public/js/mobile-adapt.js
+++ public/js/mobile-adapt.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
   var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-  var uris = {
-              card1:"geo:0,0?q=9%20Avenue%20Carnot,%2044000%20Nantes,%20France",
-              card2:"geo:0,0?q=27%20Boulevard%20de%20Stalingrad,%2044041%20Nantes,%20France",
-              card3:"geo:0,0?q=Château%20des%20ducs%20de%20Bretagne,%20Place%20Marc%20Elder,%20Nantes,%20Frances"
-              }
+  var uris = [
+                "geo:0,0?q=9%20Avenue%20Carnot,%2044000%20Nantes,%20France",
+                "geo:0,0?q=27%20Boulevard%20de%20Stalingrad,%2044041%20Nantes,%20France",
+                "geo:0,0?q=Château%20des%20ducs%20de%20Bretagne,%20Place%20Marc%20Elder,%20Nantes,%20Frances"
+             ]
   if(!iOS){
-    $('[id^=card]').each(function (index,value) {
-      $(this).click("location.href="+uris[$(this).id]);
+    console.log("passe");
+    $('img[id^=card]').each(function (index,value) {            
+      $(this).attr("onclick", "location.href='"+uris[index+1]+'\'');
     });
   }
 })
